cce466cb72794c72e4166b978a752a615fb9f5e9
snehil <snehil@192.168.1.5>
2023-06-06 22:10:50 +0530

    rendering the functioal component

--- App.js
+++ App.js
@@ -2,12 +2,25 @@ import React from "react";
 import  ReactDOM  from "react-dom/client";
 const heading1 = React.createElement("h1",{
     id : "h1tag",
+    key : "heading1"
 },"Heading1")
 const heading2 = React.createElement("h2",{
     id : "h2tag",
+    key : "heading2"
 },"Heading23")
 const div1 = React.createElement("div",{
     id : "container",
 },[heading1,heading2])
+const jsxHeading = <h1>Snehil Don</h1>
+const HeaderComponent = () =>{
+    return (
+        <div>
+        <h1>h1 tag</h1>
+        <h2>h2 tag</h2>
+        </div>
+        );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(div1) 
+// root.render(jsxHeading) 
+root.render(<HeaderComponent/>) 
+// HeaderComponent
