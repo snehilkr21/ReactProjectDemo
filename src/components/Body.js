ed5c8da6550990885000dad25c78b085dedeb2f2
snehil <snehil@192.168.1.5>
2023-06-12 00:40:37 +0530

    6th lecture

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,36 @@
 import { useEffect, useState } from "react";
-import { resturantList } from "../config";
 import ResturantCard from "./ResturantCard.js"
+import Shimmer from "./Shimmer.js";
 const Body = () =>{
-    const [resturant,setResturant] = useState(resturantList)
+    const [allResturantList,setAllResturantList] =useState([])
+    const [filteredResturant,setFilteredResturant] = useState([])
     const [searchInput,setSearchInput] = useState("")
     useEffect(()=>{
-     if(searchInput?.length==0){
-        setResturant(resturantList)
-     }
-    },[searchInput])
+      ResturantList()
+    },[])
+    async function ResturantList(){
+      let data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&page_type=DESKTOP_WEB_LISTING")
+      let json = await data.json()
+      console.log("sffew",json)
+      setAllResturantList(json?.data?.cards[2]?.data?.data?.cards)
+      setFilteredResturant(json?.data?.cards[2]?.data?.data?.cards)
+    }
     function searchFilter(searchInput,resturantList){
       return resturantList.filter((data)=>{
-        return data?.name?.includes(searchInput)
+        return data?.name?.toLowerCase()?.includes(searchInput?.toLowerCase())
       })
     }
-        return (
+
+    useEffect(()=>{
+          if(searchInput?.length ==0 ){
+            setFilteredResturant(allResturantList)
+          }
+    },[searchInput])
+
+    //early return
+    if(!allResturantList) return null;
+
+    return (allResturantList?.length === 0) ? <Shimmer/> :(
             <>
             <div className="search-container">
                 <input type="text" 
@@ -24,20 +40,22 @@ const Body = () =>{
                   onChange={(e)=>setSearchInput(e.target.value)}
                 />
                 <button className="search-btn" onClick={()=>{
-                    let data = searchFilter(searchInput,resturantList)
-                    setResturant(data)
+                    let data = searchFilter(searchInput,allResturantList)
+                    setFilteredResturant(data)
                 }
                 }
                 >Search</button>
             </div>
+            { filteredResturant?.length !==0 ? 
             <div className="ResturantList">
-            {resturant.map((item)=>{
+            {filteredResturant.map((item,index)=>{
                 return(
-                  <ResturantCard {...item} key={item.name}/>
+                  <ResturantCard {...item.data} key={item.data.name}/>
                 )
             })}
-            
             </div>
+            : <h1>No restaurants found</h1>}
+            
             </>
         );
     }
--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -1,12 +1,12 @@
-const ResturantCard = ({name,hotel}) =>{
+const ResturantCard = ({name,cuisines,cloudinaryImageId}) =>{
     // const ResturantCard = ({item}) =>{
     //     const {name, hotel} = item
     // if we want to use 89 no line and we want to destructure them then use 71,72 no line, it is easy way
         return(
             <div className='card'>
-               <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/drgphrpjbnqcwjsxaiod" />
+               <img src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`} />
                <h2>{name}</h2>
-               <h3>{hotel}</h3>
+               <h3>{cuisines.join(", ")}</h3>
                
             </div>
         )
